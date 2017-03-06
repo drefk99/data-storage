7f09e4b86fcba999d613077d606c9bb7708fc74b
drefk99 <drefk94@gmail.com>
2017-03-06 17:48:23 -0600

    I forget that

---
+++ script_bash_total.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+#echo "Hola Mundo"
+
+#cat salud.txt
+
+#variable="Hola mundo"
+#echo $variable
+
+#4 for if $1=1 lo imrpime, si es 0 imprime error, cualquier otro nada tarea
+
+#5 while if $1=1 lo imrpime, si es 0 imprime error, cualquier otro nada tarea
+
+#6 python
+
+#python -c "print('Hola mundo')"
+
+#python py_script.py
+
+#7 perl
+
+#8 parametros
+echo $1 $2 $1
