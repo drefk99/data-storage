517d2c91e7e325085ac4c2aa3e5ab58709e6825f
drefk99 <drefk94@gmail.com>
2017-03-06 17:35:07 -0600

    Socrates class

---
+++ script_bash.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#echo "Hola Mundo"
+
+#cat salud.txt
+
+#variable="Hola mundo"
+#echo $variable
+
+#4 for tarea
+
+#5 while tarea
+
+#6 python
+
+#python -c "print('Hola mundo')"
+
+#python py_script.py
+
+#7 perl
+
+
